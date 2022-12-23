fdac9c635d9b718c94fc16b37970240f90799f3a
Bruno Kelly <45316003+brunokelly@users.noreply.github.com>
2022-12-22 23:23:30 -0300

    Add project files.

---
+++ Todo.Domain/Commands/MarkTodoAsDoneCommand .cs
@@ -0,0 +1,33 @@
+﻿
+
+using Flunt.Notifications;
+using Flunt.Validations;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class MarkTodoAsDoneCommand : Notifiable<Notification>, ICommand
+    {
+        public MarkTodoAsDoneCommand() { }
+
+        public MarkTodoAsDoneCommand(Guid id, string user)
+        {
+            Id = id;
+            User = user;
+        }
+
+        public Guid Id { get; set; }
+        public string User { get; set; }
+
+        public bool Validate()
+        {
+            AddNotifications(
+                new Contract<MarkTodoAsDoneCommand>()
+                    .Requires()
+                    .IsLowerThan(User, 6, "User", "Usuário inválido!")
+            );
+
+            return IsValid;
+        }
+    }
+}
